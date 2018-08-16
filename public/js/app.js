deded61931fcad00d69a19bebb0a20dd3493f0f6
Jocie Moore <hello@jociemoore.com>
2018-08-16 12:12:23 -0700

    Update state on upvote

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,10 @@ class ProductList extends React.Component {
     super(props);
 
     this.state = {
-      products: []
-    }
+      products: [],
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
   }
 
   componentDidMount() {
@@ -12,14 +14,25 @@ class ProductList extends React.Component {
   }
 
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted');
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
   }
 
   render() {
     const products = this.state.products.sort((a,b,) => (
       b.votes - a.votes
     ));
-    const productComponents = Seed.products.map((product) => (
+    const productComponents = products.map((product) => (
         <Product 
           key={'product-' + product.id}
           id={product.id}
